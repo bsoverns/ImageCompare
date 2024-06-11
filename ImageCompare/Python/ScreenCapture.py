1f9f99f9c9788ea8f3d4e37fe60ad3d39d660cd3
Brad <bsoverns@gmail.com>
2024-06-10 22:43:15 -0400

    Minor updates

--- ImageCompare/Python/ScreenCapture.py
+++ ImageCompare/Python/ScreenCapture.py
@@ -2,40 +2,31 @@ import time
 from PIL import ImageGrab
 import datetime
 import os
-import sys
-import signal
-
-def capture_screenshots(save_path, interval=0.01):
+def capture_screenshots(interval=0.01):  
+    save_path = f"C:\Sandbox\Python\ScreenCapture\Capture"
+    # Check if the directory exists, if not, create it
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-
-    def save_screenshot():
+    try:
+        while True:
+            # Current time
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
+            filename = f'screenshot_{timestamp}.png'
+            full_path = os.path.join(save_path, filename)
+            # Screenshot
+            screen = ImageGrab.grab()
+            screen.save(full_path, 'PNG')
+            print(f'Screenshot saved as {full_path}')
+            time.sleep(interval)
+    except KeyboardInterrupt:
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f")
         filename = f'screenshot_{timestamp}.png'
         full_path = os.path.join(save_path, filename)
+        # Screenshot
         screen = ImageGrab.grab()
         screen.save(full_path, 'PNG')
         print(f'Screenshot saved as {full_path}')
-
-    def signal_handler(sig, frame):
-        print("Signal received, saving last screenshot...")
-        save_screenshot()
-        sys.exit(0)
-
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
-
-    try:
-        while True:
-            save_screenshot()
-            time.sleep(interval)
-    except KeyboardInterrupt:
-        save_screenshot()
         print("Stopped by user.")
-
+        
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python ScreenCapture.py <save_path>")
-        sys.exit(1)
-    save_path = sys.argv[1]
-    capture_screenshots(save_path)
+    capture_screenshots()
